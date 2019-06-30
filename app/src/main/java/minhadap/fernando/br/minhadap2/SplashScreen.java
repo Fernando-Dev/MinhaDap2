b30a44d835c87b61b20fb6d3e18b5a112379aed3
Fernando-Dev <fernando_dev@outlook.com.br>
2019-06-30 17:27:13 -0300

    alteracao

--- app/src/main/java/minhadap/fernando/br/minhadap2/SplashScreen.java
+++ app/src/main/java/minhadap/fernando/br/minhadap2/SplashScreen.java
@@ -1,42 +1,59 @@
 package minhadap.fernando.br.minhadap2;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashScreen extends AwesomeSplash {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+    public void initSplash(ConfigSplash configSplash) {
 
-        inicioApp();
-    }
-    private void inicioApp(){
-        new Abertura().execute();
-    }
-    private class Abertura extends AsyncTask <String,Void,String>{
 
-        @Override
-        protected String doInBackground(String... params) {
-            for (int i = 0; i < 5; i++){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            return "página carregada...";
-        }
+        //customizar o plano de fundo e sua duracao na animacao
+        configSplash.setBackgroundColor(R.color.colorPrimary); //qualquer do arquivo colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //duracao em milissgundos para duracao da animacao
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //ordem de direcao para inicio da animacao
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //ordem de direcao para inicio da animacao
 
-        @Override
-        protected void onPostExecute(String s) {
-            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-            intent.putExtra("data", s);
-            startActivity(intent);
-            finish();
-        }
+        //customizar o logo
+        configSplash.setLogoSplash(R.drawable.farm); //qualquer arquivo de drawable ou mipmap
+        configSplash.setAnimLogoSplashDuration(2000); //duracao em milissgundos para duracao da animacao
+        configSplash.setAnimLogoSplashTechnique(Techniques.DropOut); //definir o tipo de animacao que sera executado
+
+        /*
+        //customizar o pacote
+        configSplash.setPathSplash(Constantes.DROID_LOGO); //set path String
+        configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(3000);
+        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.accent); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(3000);
+        configSplash.setPathSplashFillColor(R.color.Wheat); //path object filling color
+        */
+
+        //customizar o titulo
+        configSplash.setTitleSplash("Minha Dap");// texto do titulo
+        configSplash.setTitleTextColor(R.color.Wheat);//cor o titulo
+        configSplash.setTitleTextSize(30f); //tamanho da fonte em float
+        configSplash.setAnimTitleDuration(3000);//duracao da animacao
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);//efeito no titulo
+
     }
+
+    @Override
+    public void animationsFinished() {
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
